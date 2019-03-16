f3ee9c1627a3e62531c113d964fe4c985933e6a0
Daniel Montoya <dmontoya.web@gmail.com>
2019-03-16 11:02:54 -0500

    Refactoring movements services

--- cajas/movement/views/movement_partner/create_new_partner_movements.py
+++ cajas/movement/views/movement_partner/create_new_partner_movements.py
@@ -6,9 +6,10 @@ from boxes.models.box_partner import BoxPartner
 from concepts.models.concepts import Concept, ConceptType
 from cajas.users.models.partner import PartnerType
 
-from ...models.movement_partner import MovementPartner
-from ..movement_don_juan.movement_don_juan_handler import MovementDonJuanHandler
+from ...services.don_juan_service import DonJuanManager
 from .create_movement_service_simple import CreateMovementSimpleService
+
+donjuan_manager = DonJuanManager()
 
 
 class CreateNewPartnerMovements(object):
@@ -45,7 +46,7 @@ class CreateNewPartnerMovements(object):
                 'responsible': self._responsible,
                 'ip': self._ip
             }
-            movement2 = MovementDonJuanHandler.create(data2)
+            movement2 = donjuan_manager.create_movement(data2)
         elif self._partner.partner_type == PartnerType.INDIRECTO:
             box_direct_partner = BoxPartner.objects.get(partner=self._partner.direct_partner)
             data2 = {
