cca9964f896144e97d24ca50e97114df8f4e0c31
JhonSanz <jsanz@apptitud.com.co>
2019-04-02 17:55:25 -0500

    WIP calendar

--- cajas/office/api/create_supervisor_calendar.py
+++ cajas/office/api/create_supervisor_calendar.py
@@ -1,9 +1,33 @@
+from django.contrib.auth import get_user_model
 
 from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from rest_framework import status
+
 from ..models.supervisorCalendar import SupervisorCalendar
 from ..serializer.supervisor_calendar_serializer import SupervisorCalendarSerializer
+
+from office.models.office import Office
+from units.models.units import Unit
+
+from django.shortcuts import get_object_or_404
+
+User = get_user_model()
 
 
 class CreateSupervisorCalendar(CreateAPIView):
     queryset = SupervisorCalendar.objects.all()
     serializer_class = SupervisorCalendarSerializer
+
+    def create(self, request, *args, **kwargs):
+        print(request.data)
+        SupervisorCalendar.objects.create(
+            office=get_object_or_404(Office, pk=request.data["office"]),
+            supervisor=get_object_or_404(User, pk=request.data["supervisor"]),
+            unit=get_object_or_404(Unit, pk=request.data["unit"])
+        )
+
+        return Response(
+            'Se ha creado el movimiento exitosamente.',
+            status=status.HTTP_201_CREATED
+        )
--- cajas/office/urls.py
+++ cajas/office/urls.py
@@ -6,6 +6,7 @@ from office.api.create_office_item import CreateOfficeItem
 from .api.create_supervisor_calendar import CreateSupervisorCalendar
 from office.api.office_item_delete import OfficeItemDelete
 from office.api.office_item_detail import OfficeItemDetail
+from .api.supervisor_calendar_list import SupervisorCalendarList
 
 
 app_name = 'office'
@@ -14,4 +15,5 @@ urlpatterns = [
     path("<int:pk>/office-item-detail", OfficeItemDetail.as_view(), name='office_item_detail'),
     path("office-item-create", CreateOfficeItem.as_view(), name='office_item_create'),
     path("supervisor-calendar-create", CreateSupervisorCalendar.as_view(), name='supervisor_calendar_create'),
+    path("supervisor-calendar-list", SupervisorCalendarList.as_view(), name='supervisor_calendar_list'),
 ]
