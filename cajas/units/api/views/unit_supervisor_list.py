dbaa88783814ab58de7dd2c8f1ca06c636948806
JhonSanz <jsanz@apptitud.com.co>
2019-04-03 16:46:42 -0500

    calendar url

--- cajas/units/api/views/unit_supervisor_list.py
+++ cajas/units/api/views/unit_supervisor_list.py
@@ -5,11 +5,11 @@ from ...models.units import Unit
 from ..serializers.unit_serializer import UnitSerializer
 
 
-class UnitList(generics.ListAPIView):
+class UnitSupervisorList(generics.ListAPIView):
     """
     """
 
     serializer_class = UnitSerializer
 
     def get_queryset(self):
-        return Unit.objects.filter(partner__pk=self.kwargs['pk'])
+        return Unit.objects.filter(supervisor__pk=self.kwargs['pk'])
--- cajas/units/urls.py
+++ cajas/units/urls.py
@@ -5,6 +5,7 @@ from django.urls import path
 from .api.views.unit_create import UnitCreate
 from .api.views.unit_detail import UnitDetail
 from .api.views.unit_delete import UnitDelete
+from .api.views.unit_supervisor_list import UnitSupervisorList
 from .api.views.unit_list import UnitList
 from .api.views.sell_unit import UnitSell
 
@@ -13,6 +14,7 @@ urlpatterns = [
     path("unit-create", UnitCreate.as_view(), name='unit_create'),
     path("<int:pk>/detail", UnitDetail.as_view(), name='unit_detail'),
     path("<int:pk>/delete", UnitDelete.as_view(), name='unit_delete'),
+    path("<int:pk>/supervisor-list", UnitSupervisorList.as_view(), name='unit_supervisor_list'),
     path("<int:pk>/list", UnitList.as_view(), name='unit_list'),
     path("sell/", UnitSell.as_view(), name='unit_sell'),
 ]
