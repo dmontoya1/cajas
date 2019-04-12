525b78d8f40a8ad7a0178c48a5c90b47d8d32047
JhonSanz <jsanz@apptitud.com.co>
2019-04-12 15:05:34 -0500

    WIP cierre de caja de cuadre diario

---
+++ cajas/movement/api/views/movement_daily_square/update_movement.py
@@ -0,0 +1,77 @@
+from django.shortcuts import get_object_or_404
+
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+
+from boxes.models.box_daily_square import BoxDailySquare
+from concepts.models.concepts import Concept
+from cajas.users.models.user import User
+from concepts.services.stop_service import StopManager
+from general_config.models.country import Country
+from office.models.officeCountry import OfficeCountry
+from units.models.units import Unit
+from webclient.views.get_ip import get_ip
+from webclient.views.utils import get_object_or_none
+
+from ....models.movement_daily_square import MovementDailySquare
+from ...serializers.movement_daily_square_serializer import MovementDailySquareSerializer
+
+from api.CsrfExempt import CsrfExemptSessionAuthentication
+
+
+class UpdateDailySquareMovement(generics.RetrieveUpdateAPIView):
+    queryset = MovementDailySquare.objects.all()
+    serializer_class = MovementDailySquareSerializer
+    authentication_classes = (CsrfExemptSessionAuthentication,)
+
+    def update(self, request, *args, **kwargs):
+        office_ = get_object_or_404(OfficeCountry, slug=request.POST['office_slug'])
+        box_daily_square = BoxDailySquare.objects.get(user__pk=request.POST['user_id'], office=office_)
+        concept = Concept.objects.get(pk=request.POST['concept'])
+        date = request.POST['date']
+        movement_type = request.POST['movement_type']
+        value = request.POST['value']
+        detail = request.POST['detail']
+
+        ip = get_ip(request)
+        unit = get_object_or_none(Unit, pk=request.POST.get('unit', None))
+        user = get_object_or_none(User, pk=request.POST.get('user', None))
+        country = get_object_or_none(Country, pk=request.POST.get('country', None))
+        office = get_object_or_none(OfficeCountry, pk=request.POST.get('office', None))
+
+        data = {
+            'box_daily_square': box_daily_square,
+            'unit': unit,
+            'user': user,
+            'country': country,
+            'office': office,
+            'concept': concept,
+            'movement_type': movement_type,
+            'value': value,
+            'detail': detail,
+            'date': date,
+            'responsible': request.user,
+            'ip': ip,
+        }
+        update_mvment = MovementDailySquare.objects.filter(pk=self.kwargs['pk'])
+        if user:
+            total_movements = daily_square_manager.get_user_value(data)
+            stop = StopManager.validate_stop(data)
+            if stop == 0 or (stop >= (total_movements['value__sum'] + int(data['value']))):
+                update_mvment.update(**data)
+                return Response(
+                    'Se ha creado el movimiento exitosamente',
+                    status=status.HTTP_201_CREATED
+                )
+            else:
+                return Response(
+                    'Se ha alcanzado el tope para este usuario para este concepto. No se ha creado el movimiento.',
+                    status=status.HTTP_204_NO_CONTENT
+                )
+        else:
+            update_mvment.update(**data)
+            return Response(
+                'Se ha actualizado el movimiento exitosamente',
+                status=status.HTTP_201_CREATED
+            )
--- cajas/movement/urls.py
+++ cajas/movement/urls.py
@@ -5,6 +5,7 @@ from .api.views.movement_daily_square.create_movement import CreateDailySquareMo
 from .api.views.movement_daily_square.create_withdraw_movement import CreateWithdrawMovement
 from .api.views.movement_daily_square.denied_movement import DeniedMovement
 from .api.views.movement_daily_square.dispersion_movement import DispersionMovement
+from .api.views.movement_daily_square.update_movement import UpdateDailySquareMovement
 from .api.views.movement_don_juan_usd.movement_create import MovementUSDCreate
 from .api.views.movement_office.movement_detail import MovementOfficeDetail
 from .api.views.movement_office.movement_office_create import MovementOfficeCreate
@@ -24,6 +25,7 @@ urlpatterns = [
     path("accept-daily-square-movement/", AcceptMovement.as_view(), name='accept_daily_square_movement'),
     path("denied-daily-square-movement/", DeniedMovement.as_view(), name='denied_daily_square_movement'),
     path("dispersion-movement/", DispersionMovement.as_view(), name='dispersion_movement'),
+    path("<int:pk>/list-daily-square-movement/", UpdateDailySquareMovement.as_view(), name='list_daily_square_movement'),
     path("movement-partner-create/", MovementPartnerCreate.as_view(), name='movement_partner_create'),
     path("movement-request-create/", MovementRequestCreate.as_view(), name='movement_request_create'),
     path("movement-withdraw-request-create/", MovementWithdrawRequestCreate.as_view(),
