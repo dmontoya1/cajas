066eae84c25d767723868c68112ba38c094cd528
JhonSanz <jsanz@apptitud.com.co>
2019-04-16 15:24:25 -0500

    fixed issues

--- cajas/office/api/supervisor_calendar_list.py
+++ cajas/office/api/supervisor_calendar_list.py
@@ -13,13 +13,49 @@ from cajas.users.models.group import Group
 from cajas.users.models.employee import Employee
 
 
+def get_calendar(super_calendar):
+    calendar = []
+    for item in super_calendar:
+        scheduling = {}
+        if item.assigned_date == datetime.date.today():
+            scheduling["editable"] = True
+        else:
+            scheduling["editable"] = False
+        scheduling["title"] = "Supervisor: {} \n Ruta: {} \n Socio: {} \n Cobrador: {}".format(
+            item.supervisor.first_name + " " + item.supervisor.last_name,
+            item.unit.name,
+            item.unit.partner.user.first_name + " " + item.unit.partner.user.last_name,
+            item.unit.collector.first_name + " " + item.unit.collector.last_name
+        )
+        scheduling["start"] = item.assigned_date
+        scheduling["end"] = item.assigned_date
+        scheduling["pk"] = item.pk
+        calendar.append(scheduling)
+    return calendar
+
+
 class SupervisorCalendarList(APIView):
     serializer_class = SupervisorCalendarSerializer
 
     def get(self, request, format=None):
         calendar = []
         super_calendar = []
-        user_type = request.user.related_employee.get()
+        try:
+            user_type = request.user.related_employee.get()
+        except:
+            if request.user.is_superuser:
+                super_calendar = SupervisorCalendar.objects.filter(
+                    office__slug=self.request.GET.get("office"),
+                    assigned_date__range=[
+                        datetime.date.today() - datetime.timedelta(days=5),
+                        datetime.date.today()
+                    ]
+                )
+                calendar = get_calendar(super_calendar)
+            else:
+                calendar = []
+            return JsonResponse(calendar, safe=False)
+
         if user_type.is_admin_charge():
             super_calendar = SupervisorCalendar.objects.filter(
                 office__slug=self.request.GET.get("office"),
@@ -42,21 +78,8 @@ class SupervisorCalendarList(APIView):
                 office__slug=self.request.GET.get("office"),
                 supervisor=request.user
             )
-        for item in super_calendar:
-            scheduling = {}
-            if item.assigned_date == datetime.date.today():
-                scheduling["editable"] = True
-            else:
-                scheduling["editable"] = False
-            scheduling["title"] = "Supervisor: {} \n Ruta: {} \n Socio: {} \n Cobrador: {}".format(
-                item.supervisor.first_name + " " + item.supervisor.last_name,
-                item.unit.name,
-                item.unit.partner.user.first_name + " " + item.unit.partner.user.last_name,
-                item.unit.collector.first_name + " " + item.unit.collector.last_name
-            )
-            scheduling["start"] = item.assigned_date
-            scheduling["end"] = item.assigned_date
-            scheduling["pk"] = item.pk
-            calendar.append(scheduling)
+        else:
+            super_calendar = []
 
+        calendar = get_calendar(super_calendar)
         return JsonResponse(calendar, safe=False)
