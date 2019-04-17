8ef1488e73fb2301a66d2f917bc204852e852303
JhonSanz <jsanz@apptitud.com.co>
2019-04-17 17:58:13 -0500

    WIP request movimiento entre oficinas

---
+++ cajas/movement/models/movement_between_office_request.py
@@ -0,0 +1,30 @@
+
+from django.db import models
+
+from boxes.models.box_daily_square import BoxDailySquare
+from boxes.models.box_partner import BoxPartner
+from .movement_mixin import MovementMixin
+
+
+class MovementBetweenOfficeRequest(MovementMixin):
+    """
+    """
+
+    box_office = models.ForeignKey(
+        BoxOffice,
+        verbose_name='Caja oficina',
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        related_name='movements'
+    )
+    observation = models.TextField(
+        'Observación',
+        help_text='Observación por la cual se debería de aceptar el movimiento que sobrepasó el tope'
+    )
+
+    def __str__(self):
+        return "Transpaso de dinero hacia {}".format(self.box_office.office)
+
+    class Meta:
+        verbose_name = 'Requerimiento de Movimiento entre oficinas'
+        verbose_name_plural = 'Requerimientos de Movimientos entre oficinas'
