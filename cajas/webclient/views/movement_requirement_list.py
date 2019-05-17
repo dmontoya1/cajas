34b34ed904c34f75f95824deb16a8e7d4eaa98f9
JhonSanz <jsanz@apptitud.com.co>
2019-05-17 09:45:58 -0500

    ajuste del menu home

--- cajas/webclient/views/movement_requirement_list.py
+++ cajas/webclient/views/movement_requirement_list.py
@@ -3,6 +3,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 from cajas.movement.models.movement_request import MovementRequest
+from cajas.office.models.officeCountry import OfficeCountry
+from cajas.users.models.partner import Partner
 
 
 class MovementRequireList(LoginRequiredMixin, TemplateView):
@@ -17,4 +19,6 @@ class MovementRequireList(LoginRequiredMixin, TemplateView):
         context = super(MovementRequireList, self).get_context_data(**kwargs)
         movements = MovementRequest.objects.all().exclude(concept__name='Retiro Socio Directo')
         context['movements'] = movements
+        context['all_offices'] = OfficeCountry.objects.all().order_by('office')
+        context['partners_offices'] = Partner.objects.all().exclude(code='DONJUAN')
         return context
