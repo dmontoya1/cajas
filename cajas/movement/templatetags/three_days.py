c634a0fa59810e17a819ae609c014977d5a73cdb
Daniel Montoya <dmontoya_lab@gmail.com>
2019-05-21 08:09:01 -0500

    Updating template tag

--- cajas/movement/templatetags/three_days.py
+++ cajas/movement/templatetags/three_days.py
@@ -7,7 +7,7 @@ register = template.Library()
 
 @register.filter()
 def three_last_days(a_date):
-    today = date.today()-timedelta(days=2)
+    today = date.today()-timedelta(days=3)
     if a_date >= today:
         return True
     return False
