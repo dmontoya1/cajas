c3fe4bf2a914d25e40d8ccddfac5a6b7fde218de
Daniel Montoya <dmontoya_lab@gmail.com>
2019-05-22 16:35:49 -0500

    Updating closeout boxes list permissions

--- cajas/webclient/views/closeout_boxes.py
+++ cajas/webclient/views/closeout_boxes.py
@@ -20,7 +20,10 @@ class CloseoutBoxesList(LoginRequiredMixin, TemplateView):
         context = super(CloseoutBoxesList, self).get_context_data(**kwargs)
         slug = self.kwargs['slug']
         office = get_object_or_404(OfficeCountry, slug=slug)
-        boxes = BoxPartner.objects.filter(Q(box_status=BoxStatus.EN_LIQUIDACION) | Q(box_status=BoxStatus.LIQUIDADA))
+        boxes = BoxPartner.objects.filter(
+            (Q(box_status=BoxStatus.EN_LIQUIDACION) | Q(box_status=BoxStatus.LIQUIDADA)) &
+            Q(partner__office=office)
+        )
         context['boxes'] = boxes
         context['office'] = office
         return context
