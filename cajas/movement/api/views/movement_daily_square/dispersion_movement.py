b105dc52baa7890ef5537426f797f45161f4fb62
Daniel Montoya <dmontoya_lab@gmail.com>
2019-05-24 14:46:32 -0500

    Adding box office on DQ dispersion

--- cajas/movement/api/views/movement_daily_square/dispersion_movement.py
+++ cajas/movement/api/views/movement_daily_square/dispersion_movement.py
@@ -6,15 +6,16 @@ from rest_framework.response import Response
 
 from django.shortcuts import get_object_or_404
 
-from cajas.boxes.models.box_don_juan import BoxDonJuan
-from cajas.users.models.partner import Partner
 from cajas.api.CsrfExempt import CsrfExemptSessionAuthentication
-from cajas.movement.models.movement_daily_square import MovementDailySquare
-from cajas.movement.services.don_juan_service import DonJuanManager
-from cajas.movement.services.partner_service import MovementPartnerManager
+from cajas.boxes.models import BoxDonJuan, BoxOffice
+from cajas.users.models.partner import Partner
 from cajas.webclient.views.get_ip import get_ip
+from cajas.office.models import OfficeCountry
 
 from ....models.movement_daily_square import MovementDailySquare
+from ....services.don_juan_service import DonJuanManager
+from ....services.office_service import MovementOfficeManager
+from ....services.partner_service import MovementPartnerManager
 
 
 class DispersionMovement(APIView):
@@ -29,9 +30,8 @@ class DispersionMovement(APIView):
             counter = request.data['counter'] / 2
             counter = int(counter)
             for i in range(counter):
-                value = request.data["form[form]["+str(i)+"][value]"]
-                partner = get_object_or_404(Partner, pk=request.data["form[form]["+str(i)+"][partner]"])
                 ip = get_ip(request)
+                value = request.data["form[form][" + str(i) + "][value]"]
                 data = {
                     'concept': movement.concept,
                     'movement_type': movement.movement_type,
@@ -41,14 +41,25 @@ class DispersionMovement(APIView):
                     'responsible': request.user,
                     'ip': ip
                 }
-                if partner.code == 'DONJUAN':
-                    don_juan_manager = DonJuanManager()
-                    data['box'] = BoxDonJuan.objects.get(office__slug=request.data['office'])
-                    don_juan_manager.create_movement(data)
+                print("form[form]["+str(i)+"][partner]")
+                if request.data["form[form]["+str(i)+"][partner]"] == "office":
+                    print("Entro al if")
+                    office = OfficeCountry.objects.get(slug=request.data['office'])
+                    office_manager = MovementOfficeManager()
+                    data['box_office'] = BoxOffice.objects.get(office=office)
+                    office_manager.create_movement(data)
                 else:
-                    data['box'] = partner.box
-                    movement_partner_manager = MovementPartnerManager()
-                    movement1 = movement_partner_manager.create_simple(data)
+                    print("ELSEEE")
+                    partner = get_object_or_404(Partner, pk=request.data["form[form]["+str(i)+"][partner]"])
+
+                    if partner.code == 'DONJUAN':
+                        don_juan_manager = DonJuanManager()
+                        data['box'] = BoxDonJuan.objects.get(office__slug=request.data['office'])
+                        don_juan_manager.create_movement(data)
+                    else:
+                        data['box'] = partner.box
+                        movement_partner_manager = MovementPartnerManager()
+                        movement_partner_manager.create_simple(data)
 
             movement.review = True
             movement.status = MovementDailySquare.DISPERSED
