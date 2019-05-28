a3c15880eb4d61ef42e965d692e4d6819b3b1d80
Daniel Montoya <dmontoya_lab@gmail.com>
2019-05-28 11:04:24 -0500

    Hot Fix Permission to add and edit movement

--- cajas/users/models/employee.py
+++ cajas/users/models/employee.py
@@ -1,7 +1,5 @@
 
 from django.contrib.auth import get_user_model
-from django.conf import settings
-from django.contrib.sites.models import Site
 from django.db import models
 
 from cajas.users.models.charges import Charge
