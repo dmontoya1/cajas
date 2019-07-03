aef8edf9d133bdf5cdd0f0c11254fbe9ffcc898e
Daniel Montoya <dmontoya_lab@gmail.com>
2019-07-02 20:40:50 -0500

    Updating sell unit when seller is DonJuan

--- cajas/units/api/views/sell_unit.py
+++ cajas/units/api/views/sell_unit.py
@@ -42,22 +42,40 @@ class UnitSell(APIView):
         unit.save()
 
         ip = get_ip(request)
-        data_seller = {
-            'box': seller_partner.box,
-            'concept': concept.counterpart,
-            'date': datetime.now(),
-            'movement_type': 'IN',
-            'value': price,
-            'detail': 'Venta de unidad {} al socio {}. Precio inventario: ${} - Precio de venda+: ${}'.format(
-                unit.name,
-                buyer_partner.get_full_name(),
-                request.data['price_items'],
-                request.data['unit_price']
-            ),
-            'responsible': request.user,
-            'ip': ip,
-        }
-        movement_partner_manager.create_simple(data_seller)
+        if seller_partner.code == 'DONJUAN':
+            data_seller = {
+                'box': BoxDonJuan.objects.get(office=seller_partner.office),
+                'concept': concept.counterpart,
+                'date': datetime.now(),
+                'movement_type': 'IN',
+                'value': price,
+                'detail': 'Venta de unidad {} al socio {}. Precio inventario: ${} - Precio de venda+: ${}'.format(
+                    unit.name,
+                    seller_partner.get_full_name(),
+                    request.data['price_items'],
+                    request.data['unit_price']
+                ),
+                'responsible': request.user,
+                'ip': ip,
+            }
+            don_juan_manager.create_movement(data_seller)
+        else:
+            data_seller = {
+                'box': seller_partner.box,
+                'concept': concept.counterpart,
+                'date': datetime.now(),
+                'movement_type': 'IN',
+                'value': price,
+                'detail': 'Venta de unidad {} al socio {}. Precio inventario: ${} - Precio de venda+: ${}'.format(
+                    unit.name,
+                    buyer_partner.get_full_name(),
+                    request.data['price_items'],
+                    request.data['unit_price']
+                ),
+                'responsible': request.user,
+                'ip': ip,
+            }
+            movement_partner_manager.create_simple(data_seller)
         if buyer_partner.code == 'DONJUAN':
             data_buyer = {
                 'box': BoxDonJuan.objects.get(office=seller_partner.office),
