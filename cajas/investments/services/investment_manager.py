cf8a2a5bcd7640914b08e4f1564f489f49e04a92
Daniel Montoya <dmontoya.lab@gmail.com>
2019-09-20 15:05:48 -0500

    Ajuste en creación de Inversión
    Cuando se crea la inversión, y tiene cuota inicial, crear los movimientos pertinentes

--- cajas/investments/services/investment_manager.py
+++ cajas/investments/services/investment_manager.py
@@ -1,16 +1,42 @@
 
 from django.shortcuts import get_object_or_404
 
+from cajas.concepts.models.concepts import Concept, ConceptType
+from cajas.movement.services.partner_service import MovementPartnerManager
 from cajas.users.models.partner import Partner
-from ..models.investment import Investment
-from ..services.investment_pay_manager import InvestmentPayManager
+from cajas.webclient.views.get_ip import get_ip
 
-pay_manager = InvestmentPayManager()
+from ..models.investment import Investment
+from ..models.investment_pay import InvestmentPay
 
 
 class InvestmentManager(object):
 
-    def investment_create(self, data):
+    def investment_create(self, request):
+        investment = self.create_object(request.data)
+        data = request.data
+        if int(data['initial_value']) > 0 and investment.investment_type == investment.BUSINESS:
+            self.create_payment(request, investment)
+            investment.balance = int(investment.balance) - int(request.data['initial_value_cop'])
+            investment.save()
+
+            concept = get_object_or_404(Concept, name='Inversión Negocios', concept_type=ConceptType.DOUBLE)
+            data = {
+                'partner': investment.partner,
+                'box': investment.partner.box,
+                'concept': concept,
+                'movement_type': 'OUT',
+                'value': request.data['initial_value'],
+                'detail': "Pago cuota inicial {}".format(investment),
+                'date': request.data['date'],
+                'responsible': request.user,
+                'ip': get_ip(request)
+            }
+            movement_partner_manager = MovementPartnerManager()
+            return movement_partner_manager.create_double(data)
+        return investment
+
+    def create_object(self, data):
         investment = Investment.objects.create(
             partner=get_object_or_404(Partner, pk=data['partner']),
             date=data['date'],
@@ -22,3 +48,12 @@ class InvestmentManager(object):
             investment_type=data['investment_type']
         )
         return investment
+
+    def create_payment(self, request, investment):
+        InvestmentPay.objects.create(
+            investment=investment,
+            value=request.data['initial_value'],
+            value_cop=request.data['initial_value_cop'],
+            date=request.data['date'],
+            detail="Pago cuota inicial {}".format(investment),
+        )
