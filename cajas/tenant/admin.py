89f04b1668e0ffcb581bfb08f3457c760f33a75e
Daniel Montoya <dmontoya.lab@gmail.com>
2019-09-25 16:17:24 -0500

    Deleting temporaly admin tenant

--- cajas/tenant/admin.py
+++ cajas/tenant/admin.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from .models import Platform
 
 
-@admin.register(Platform)
+# @admin.register(Platform)
 class TenantAdmin(admin.ModelAdmin):
 
     list_display = ('name', 'schema_name', 'domain_url')
